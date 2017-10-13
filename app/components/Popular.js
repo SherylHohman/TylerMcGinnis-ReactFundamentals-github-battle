7e1d45453c371fb6f5547e685c5670c3d4b97a56
SherylHohman <SherylHohman@users.noreply.github.com>
2017-10-12 20:57:01 -0700

    add 'Popular' component (stump)

---
+++ app/components/Popular.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+class Popular extends React.Component{
+  render(){
+    return (
+      <div> Popular Languages Menu Goes Here </div>
+    )
+  }
+};
+
+module.exports = Popular;
